6f89ddc7203c5ede83da18dbafcb081e666b198b
Miikka Valkeapaa <miikka1973@gmail.com>
2018-01-16 21:01:26 +0200

    more additions to test.py

--- test.py
+++ test.py
@@ -12,10 +12,49 @@ conversion = round(int(temperature)*9/5+32)
 
 print("Temperature in Fahrenheit: "+str(conversion))
 
-'''
+
 
 age = input("Provide age: ")
 size = input("Provide size: ")
 
 print("You are {myage} years old and have the {mysize} shoe size".format(myage=age,mysize=size))
 
+
+import random
+min_int = 100
+
+for i in range(100):
+    rand_int = random.randint(0,100)
+    if rand_int <= min_int:
+        min_int = rand_int
+        print("Minimium number is now set to: {}".format(min_int))
+print("The final minimal number is: "+ str(min_int))
+
+
+import random
+magic_numbers = [ random.randint(0, 9), random.randint(0, 9) ]
+
+def prompt_user():
+    number_var = input("Try to guess the magic number: ")
+    if int(number_var) in magic_numbers:
+        return "You guessed it!"
+    else:
+        return "You didn´t guess it"
+
+def the_loop(attempts):
+    for i in range(attempts):
+        print(prompt_user())
+
+user_attempts = int(input("How many times to loop: "))
+the_loop(user_attempts)
+
+print("These are the magic numbers: {}".format(magic_numbers))
+
+
+def my_method(input_number, divisor):
+    if divisor == 0:
+        return None
+    return input_number / divisor
+print(my_method(8000,0))
+
+'''
